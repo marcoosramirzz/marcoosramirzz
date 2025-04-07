f8f28a05d935271518ae986f0417403c306416bd
marcoosramirzz <marcosrami907@gmail.com>
2025-04-07 22:48:58 +0200

    Primera versión de mi sitio web

---
+++ script.js
@@ -0,0 +1,5 @@
+document.querySelector('form').addEventListener('submit', function(e) {
+    e.preventDefault();
+    alert('Formulario enviado. Nos pondremos en contacto contigo pronto.');
+    // Aquí puedes añadir código para manejar el envío del formulario, como enviarlo a un servidor
+});
